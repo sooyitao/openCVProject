42b2d4f82e3d780c1b66eb436aea87d4b525b50a
sooyitao <sooyitao@gmail.com>
2024-10-12 06:18:44 +0800

    Implemented circle detection

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 import cv2
+import numpy as np
 
 # Load an image
-img = cv2.imread('AimTrainer.jpg')
-cv2.imshow('Image', img)
+img = cv2.imread('AimTrainer.jpg', cv2.IMREAD_COLOR)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray_blurred = cv2.blur(gray, (3, 3))
+
+# Apply Hough transform on the blurred image. 
+detected_circles = cv2.HoughCircles(gray_blurred,  
+                   cv2.HOUGH_GRADIENT, 1, 20, param1 = 50, 
+               param2 = 30, minRadius = 40, maxRadius = 60) 
+
+# Draw circles that are detected. 
+if detected_circles is not None: 
+  
+    # Convert the circle parameters a, b and r to integers. 
+    detected_circles = np.uint16(np.around(detected_circles)) 
+  
+    for pt in detected_circles[0, :]: 
+        a, b, r = pt[0], pt[1], pt[2] 
+  
+        # Draw the circumference of the circle. 
+        cv2.circle(img, (a, b), r, (0, 255, 0), 2) 
+  
+        # Draw a small circle (of radius 1) to show the center. 
+        cv2.circle(img, (a, b), 1, (0, 0, 255), 3) 
+        cv2.imshow("Detected Circle", img)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
